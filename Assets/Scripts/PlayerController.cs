c5455b10cf2426ac2b807ad6f04397f3efdd0bc4
Sarahpixel <107088382+Sarahpixel@users.noreply.github.com>
2022-06-15 11:56:14 +1000

    pickup and win condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 1.0f;
-    // Start is called before the first frame update
+    public int pickupCount;
+    private bool wonGame = false;
     void Start()
     {
         //gets the rigidbody componet attached to this game object
         rb = GetComponent<Rigidbody>();
+        // Work out how many pickups are in the scene and store in (pickup count)
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Display the pickups to the user
+        CheckPickups();
+
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
+        //if we have won the game, return the function
+        if (wonGame == true)
+            return;
+
         //store the horizontal axis value in a float
         float moveHorizontal = Input.GetAxis("Horizontal");
         //store the vertical axis value in a float
@@ -28,4 +38,48 @@ public class PlayerController : MonoBehaviour
         //add force to our rigidbody from our movement multiplying the movement by a number
         rb.AddForce(movement* 5);
     }
-}
+
+   
+     private void OnTriggerEnter(Collider other)
+     {
+        //if we collide witha pickup, destroy the pickup
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            //Decrement the pickupcount when we collide with the pickup
+            pickupCount -= 1;
+            //Display the pickups to the user
+            CheckPickups();
+
+            Destroy(other.gameObject);
+        } 
+     }
+    void CheckPickups()
+    {
+        //Display the pickupcount to the player
+          Debug.Log("Pickup Count:" + pickupCount);
+        //Check if the pickupcount ==0 
+         if (pickupCount==0)
+         {
+            //if pickup count ==0 display win message, remove contols from player??
+            Debug.Log("You Win");
+            //remove the player controls
+            wonGame = true;
+            //set the velocity of the rigid body to zero
+            rb.velocity = Vector3.zero;
+         }
+
+    }
+  
+
+    
+
+    
+
+    //Create a win condition that happens when pickupcount == 0
+
+
+    
+
+    
+        
+}   
